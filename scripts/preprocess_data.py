92f5ba2099424252d24a82213b9a6abbbf0a1a3b
MRyutaro <m2002r1028@icloud.com>
2024-08-26 15:29:13 +0900

    add: init

---
+++ scripts/preprocess_data.py
@@ -0,0 +1,5 @@
+"""
+データの前処理を行うスクリプト
+
+server.pyでimportして、model_analysis/main.pyやmodel_build/main.pyに関数渡しをして使う
+"""
---
+++ server.py
@@ -0,0 +1,5 @@
+"""
+APIサーバ
+
+Reactでビルドした後、/frontend/build/index.htmlを返すことでReactで作った画面を表示できる。
+"""

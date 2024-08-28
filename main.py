89564a8d09b5b3e201090598c887e7ac95c277a3
MRyutaro <m2002r1028@icloud.com>
2024-08-28 21:39:37 +0900

    add: コマンド版

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+from models_analysis.main import analyze_video
+
+if __name__ == "__main__":
+    current_dir = os.path.dirname(__file__)
+    models_dir = os.path.join(current_dir, "models")
+
+    # python main.py uploads/hoko.mp4みたいな感じで実行
+    try:
+        video_path = sys.argv[1]
+        analyze_video(video_path, models_dir)
+    except IndexError:
+        print("python main.py {video_path}で実行してください")
+        sys.exit(1)
